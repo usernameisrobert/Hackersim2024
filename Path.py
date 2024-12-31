d375fa126f6faf8ad5d2e268647cdb031b7ee030
levi <levidalrymple1221@gmail.com>
2024-12-31 11:18:54 -0600

    Adds basic path class

--- Path.py
+++ Path.py
@@ -1,8 +1,20 @@
+from Player import Player
 class Path:
-    def __init__(self, description, connected, on_call):
+    def __init__(self, description, paths, on_call, auto_input: bool = True):
         self.description = description
-        self.connected = connected
+        self.paths = paths
         self.on_call = on_call
+        self.auto_input = auto_input
 
-    def enter(self, player):
-        pass
+    @staticmethod
+    def get_input(prompt='> ') -> int:
+        while True:
+            try:
+                return int(input(prompt))
+            except Exception:
+                print("Must enter a number")
+
+    def enter(self, player: Player):
+        print(self.description)
+        val: int | None = self.get_input() if self.auto_input else None
+        self.on_call(player, self.paths, val)
