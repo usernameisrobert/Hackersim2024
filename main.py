601295fd53ab392dde04938c23e76b546cda9c91
levi <levidalrymple1221@gmail.com>
2024-12-30 20:13:34 -0600

    Refactor code to make more smoother and better intro scene

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
 import time
 import random
 import os
+import platform
 
 colors = ["\033[32m","\033[90m","\033[34m"]
+reset = "\033[0m"
 
-name = input("name: ")
 
-print(f"YOU ARE {name}\nhealth: 100\nmoney: 20\ncan you guess a number between 1 and 5")
-
-number = random.randint(1, 5)
-
-guess = 0
-
-while guess != number:
-    try:
-        guess = int(input("guess: "))
-    except:
-        pass
 
 text=""
 
-for frame in range(30):
-    rows, columns = os.get_terminal_size()
-    for y in range(columns):
-        for x in range(rows):
-            if (y == round(rows/2)) and ():
-                
-            print(colors[random.randint(0,2)] + chr(random.randint(32, 126)), end='')
-        print()
-    time.sleep(0.1)
+def clear():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def intro_scene():
+    clear()
+    buf = ''
+    rows, cols = os.get_terminal_size()
+    CHAR_SIZE = 5 # 4 for ansi color 1 for character
+    while True:
+        for y in range(0, cols):
+            for x in range(0, rows):
+                color = colors[random.randint(0,len(colors)-1)]
+                character = chr(random.randint(32, 126))
+                buf += color + character
+            buf += '\n'
+        print(buf)
+        time.sleep(0.1)
+
+    # now we need to print the text centered of screen
+
+def main():
+    name = input("name: ")
+
+    print(f"YOU ARE {name}\nhealth: 100\nmoney: 20\ncan you guess a number between 1 and 5")
+
+    number = random.randint(1, 5)
+    guess = 0
+    while guess != number:
+        try:
+            guess = int(input("guess: "))
+        except Exception:
+            print("You must enter a number")
+
+    intro_scene()
+
+if __name__ == "__main__":
+    main()
