7a3f864a983a354856b1746784a410271726749b
levi <levidalrymple1221@gmail.com>
2024-12-30 21:05:40 -0600

    Adds intro scene, comp: windows (maybe mac), linux

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ import platform
 colors = ["\033[32m","\033[90m","\033[34m"]
 reset = "\033[0m"
 
-
-
-text=""
+text="HACKERSIM 2024"
 
 def clear():
     if platform.system() == "Windows":
@@ -18,20 +16,9 @@ def clear():
 
 def intro_scene():
     clear()
-    buf = ''
-    rows, cols = os.get_terminal_size()
-    CHAR_SIZE = 5 # 4 for ansi color 1 for character
-    while True:
-        for y in range(0, cols):
-            for x in range(0, rows):
-                color = colors[random.randint(0,len(colors)-1)]
-                character = chr(random.randint(32, 126))
-                buf += color + character
-            buf += '\n'
-        print(buf)
-        time.sleep(0.1)
-
-    # now we need to print the text centered of screen
+    path = f"scripts/{platform.system()}/Intro"
+    # we want to run this file as a executable and wait for it to finish
+    os.system(path)
 
 def main():
     name = input("name: ")
@@ -47,6 +34,8 @@ def main():
             print("You must enter a number")
 
     intro_scene()
+    clear()
+    print("The end")
 
 if __name__ == "__main__":
     main()
